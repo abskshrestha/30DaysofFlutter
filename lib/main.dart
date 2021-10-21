4dd89679b0eb091f89e350897306c05d77f91f9d
abskshrestha <81076450+abskshrestha@users.noreply.github.com>
2021-10-21 19:47:23 +0545

    day 9 completed

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,11 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.deepPurple,
         fontFamily: GoogleFonts.lato().fontFamily,
       ),
-      darkTheme: ThemeData(brightness: Brightness.light),
+      darkTheme: ThemeData(
+        brightness: Brightness.light
+        ),
+        debugShowCheckedModeBanner: false,
+      initialRoute: MyRoutes.homeRoute,
       routes: {
         "/": (context) => LoginPage(),
         MyRoutes.homeRoute: (context) => HomePage(),
