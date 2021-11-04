840c83ecd1872d014183e1e28d1c600c006fb61f
abskshrestha <81076450+abskshrestha@users.noreply.github.com>
2021-11-04 19:52:05 +0545

    data class generator

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,33 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_catalouge/models/catalouge.dart';
 import 'package:flutter_catalouge/widgets/drawer.dart';
 import 'package:flutter_catalouge/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
-  final int days = 30;
-  final String name = "codepur";
-
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final int days = 30;
+
+  final String name = "codepur";
+
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  // loadData() async {
+  //  var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+  //  var decodedData = jsonDecode(catalogJson);
+  //  print(decodedData);
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(20, (index) => CatalougeModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Catalouge App",
+          "Catalog App",
         ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: dummyList.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(
-              item: dummyList[index],
-            );
-          },
-        ),
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+            ? ListView.builder(
+                itemCount: CatalogModel.items.length,
+                itemBuilder: (context, index) {
+                  return ItemWidget(
+                    item: CatalogModel.items[index],
+                  );
+                },
+              )
+            : Center(child: CircularProgressIndicator()),
       ),
       drawer: MyDrawer(),
     );
