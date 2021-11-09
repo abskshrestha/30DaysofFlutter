c43d801dd125395e039ed62207de3ecc15c4a741
abskshrestha <81076450+abskshrestha@users.noreply.github.com>
2021-11-09 18:31:08 +0545

    day 5 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,7 +13,7 @@ class HomePage extends StatelessWidget {
         title: Text("Catalouge App"),
       ),
       body: Center(
-        child: Text('Welcome to $days of flutter by $name'),
+        child: Text('Welcome to $days off flutter by $name'),
       ),
       drawer: Drawer(),
     );
