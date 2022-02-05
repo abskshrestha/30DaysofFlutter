7789be15f4180a4f3fc84ec0f4a97159a27acfc3
Abhishek Shrestha <absk.shrestha@gmail.com>
2022-02-05 20:08:02 +0545

    day 17 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'dart:convert';
+import 'package:velocity_x/velocity_x.dart';
+
 import 'package:flutter_catalouge/models/catalouge.dart';
 import 'package:flutter_catalouge/widgets/drawer.dart';
 import 'package:flutter_catalouge/widgets/item_widget.dart';
+import 'package:flutter_catalouge/widgets/themes.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -42,45 +46,111 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Catalog App",
+
+        body: SafeArea(
+      child: Container(
+        padding: Vx.m32,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CatalogHeader(),
+            if (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+              CatalogList().expand()
+            else
+              Center(
+                child: CircularProgressIndicator(),
+              ),
+          ],
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-            ? GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 16,
-                  crossAxisSpacing: 16), 
-                itemBuilder: (context,index){
-                  final item = CatalogModel.items[index];
-                  return Card(
-                    clipBehavior: Clip.antiAlias,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                    child: GridTile(
-                      header: Container(
-                        child: Text(
-                          item.name, 
-                          style: TextStyle(color: Colors.white)),
-                        padding: const EdgeInsets.all(12),
-                        decoration: BoxDecoration(
-                          color: Colors.deepPurple,
-                        ),
-                        ),
-                      child: Image.network(item.image),
-                      footer: Text(item.price.toString()),)
-                  );
-                },
+    ));
+  }
+}
 
-                itemCount: CatalogModel.items.length,)
+class CatalogHeader extends StatelessWidget {
+  const CatalogHeader({Key? key}) : super(key: key);
 
-            : Center(child: CircularProgressIndicator()),
-      ),
-      drawer: MyDrawer(),
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        "Catalog App".text.xl5.bold.color(MyTheme.darkBluishColor).make(),
+        "Trending Products".text.xl2.make(),
+      ],
     );
+  }
+}
+
+class CatalogList extends StatelessWidget {
+  const CatalogList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: CatalogModel.items.length,
+      itemBuilder: (context, index) {
+        final catalog = CatalogModel.items[index];        
+        return CatalogItem(Catalog: catalog,);
+      },
+    );
+  }
+}
+
+class CatalogItem extends StatelessWidget {
+  final Item Catalog; //import
+  const CatalogItem(
+      {Key? key, required this.Catalog})
+      : assert(Catalog != null),
+        super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+final String image;
+    return VxBox(
+      child: Row(
+        children: [
+          CatalogImage(
+            image: Catalog.image,),
+            Expanded(child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Catalog.name.text.lg.color(MyTheme.darkBluishColor).bold.make(),
+                Catalog.desc.text.sm.make(),
+                10.heightBox,
+                ButtonBar(
+                  alignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    "\$${Catalog.price}".text.bold.xl.make(),
+                    ElevatedButton(onPressed: (){},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(MyTheme.darkBluishColor),
+                    shape: MaterialStateProperty.all(StadiumBorder(),),
+                    ),
+                     child: "Buy".text.make())
+                  ],
+                  
+                ).pOnly(right: 8.0),
+                ],
+                ),
+                )
+        ],)
+    ).white.rounded.square(150).make().py16();
+  }
+}
+
+class CatalogImage extends StatelessWidget {
+  const CatalogImage({
+    Key? key,
+    required this.image,
+  }) : super(key: key);
+final String image; 
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      image,
+          ).box.rounded.p8.color(MyTheme.creamColor).make().py16().w40(context);
   }
 }
--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 class MyTheme{
   static ThemeData LightTheme(BuildContext context) => ThemeData(
         primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
+        fontFamily: GoogleFonts.poppins().fontFamily,
         appBarTheme: AppBarTheme(
           color: Colors.black,
           elevation: 0.0,
@@ -17,4 +17,7 @@ class MyTheme{
         brightness: Brightness.dark,
         );
       
+      //colors
+      static Color creamColor = Color(0XFFFF5F5);
+      static Color darkBluishColor = Color(0xff403b58);
 }
